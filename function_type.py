39637dd7dbd5a06d94c31c5822aca0b73653b280
rjanssen96 <janssenrico@home.nl>
2018-12-17 10:44:40 +0100

    homogeneous steps

--- function_type.py
+++ function_type.py
@@ -1,8 +1,9 @@
 def type():
     print("We will determine the type of the equation...")
     try:
-
-    type_equation = "homogeneous"
-    error_file = "file_path"
+        type_equation = "homogeneous"
+        error_file = "file_path"
+    except:
+        print("ERROR")
 
     return type_equation
