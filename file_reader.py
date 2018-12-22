2014d9bfa461155c0af0cf1cb65db7f879496dfe
rjanssen96 <janssenrico@home.nl>
2018-12-22 19:16:12 +0100

    File reader included in main menu, start function created in file_reader.py

--- file_reader.py
+++ file_reader.py
@@ -184,44 +184,46 @@ def det_coefficients(equation):
         print(coeff_sorted_list)
 
 
-    # def write_coefficents_to_file(filename, coefficients):
-path = str(os.path.dirname(os.path.realpath(__file__)) + "/input_files/comass[0-9][0-9].txt")
-for filename in glob.glob(path):
-    print("File: " + filename)
-    # next_symbolic_var_index = 0  # Reset this index for every file
-    # debug_print("Beginning for file \"{0}\"".format(filename))
-    lines = read_file(filename)
-    lines = clear_commas(lines)
-    lines = fix_syntax(lines)
-    print("Len lines: " + str(len(lines)))
-    print(lines)
-    # debug_print(lines)
-    # # The following quick fix was done because some input files had two newlines at their end and the list "lines" thus may contain one empty line "" at the end
-    tmp = len(lines)
-    if lines[len(lines) - 1] == "":
-        tmp -= 1
-    init_conditions = det_init_conditions(
-        [lines[index] for index in range(1, tmp)])  # Determine initial conditions with all but the first line as input
-    associated, f_n_list = analyze_recurrence_equation(lines[0])
+def read_files():
+        # def write_coefficents_to_file(filename, coefficients):
+    path = str(os.path.dirname(os.path.realpath(__file__)) + "/input_files/comass[0-9][0-9].txt")
+    global filename #The filename needs to be available in every function.
+    for filename in glob.glob(path):
+        print("File: " + filename)
+        # next_symbolic_var_index = 0  # Reset this index for every file
+        # debug_print("Beginning for file \"{0}\"".format(filename))
+        lines = read_file(filename)
+        lines = clear_commas(lines)
+        lines = fix_syntax(lines)
+        print("Len lines: " + str(len(lines)))
+        print(lines)
+        # debug_print(lines)
+        # # The following quick fix was done because some input files had two newlines at their end and the list "lines" thus may contain one empty line "" at the end
+        tmp = len(lines)
+        if lines[len(lines) - 1] == "":
+            tmp -= 1
+        init_conditions = det_init_conditions(
+            [lines[index] for index in range(1, tmp)])  # Determine initial conditions with all but the first line as input
+        associated, f_n_list = analyze_recurrence_equation(lines[0])
 
-    det_coefficients(equation=lines)
-    # # Print debugging information:
-    # debug_print(filename)
-    # debug_print("Initial conditions:")
-    # debug_print(init_conditions)
-    # debug_print("Associated homogeneous recurrence relation:")
-    # debug_print(associated)
-    # debug_print("F(n):")
-    # debug_print(f_n_list)
-    #
-    # output_filename = filename.replace(".txt", "-dir.txt")
-    # resulting_equ = ""
-    # # Check if the equation is a homogeneous relation
-    # if not f_n_list:  # The list is empty
-    #     resulting_equ = solve_homogeneous_equation(init_conditions, associated)
-    # else:
-    #     resulting_equ = solve_nonhomogeneous_equation(init_conditions, associated, f_n_list)
-    # resulting_equ = reformat_equation(resulting_equ)
-    # write_output_to_file(output_filename, resulting_equ)
-    #
-    # debug_print("#################################\n")
+        det_coefficients(equation=lines)
+        # # Print debugging information:
+        # debug_print(filename)
+        # debug_print("Initial conditions:")
+        # debug_print(init_conditions)
+        # debug_print("Associated homogeneous recurrence relation:")
+        # debug_print(associated)
+        # debug_print("F(n):")
+        # debug_print(f_n_list)
+        #
+        # output_filename = filename.replace(".txt", "-dir.txt")
+        # resulting_equ = ""
+        # # Check if the equation is a homogeneous relation
+        # if not f_n_list:  # The list is empty
+        #     resulting_equ = solve_homogeneous_equation(init_conditions, associated)
+        # else:
+        #     resulting_equ = solve_nonhomogeneous_equation(init_conditions, associated, f_n_list)
+        # resulting_equ = reformat_equation(resulting_equ)
+        # write_output_to_file(output_filename, resulting_equ)
+        #
+        # debug_print("#################################\n")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #This programs triggers all the other scripts.
-
 import os
 import function_type
+import file_reader
 def banner():
     path = os.path.dirname(os.path.realpath(__file__))
     print("This program is written by ...\n")
@@ -14,7 +14,6 @@ def banner():
     print("Step 3: \n")
     print("Step 4: \n")
     print("Step 5: \n")
-    print("Step 6: \n")
 
     print("\nFor non-homogeneous equations we follow the following steps:\n")
     print("Step 1: \n")
@@ -23,14 +22,21 @@ def banner():
     print("Step 4: \n")
     print("Step 5: \n")
     print("Step 6: \n")
+    print("Step 7: \n")
 
     print("\n**Note that the input files should be in {}/input_files\n".format(path))
 
+def read_files():
+    print("We are going to read all the files...\n")
+    try:
+        file_reader.read_files()
+    except Exception as error:
+        print("Something went wrong! \nError: {}".format(error))
 
-def menu ():
+def menu():
     #This function is the main menu for the program.
     banner()
-
+    read_files()
     #Determine if the function is an homogeneous or a non-homogeneous function
     try:
         if function_type.type() == "homogeneous":
