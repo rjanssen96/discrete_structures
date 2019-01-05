96a72fc25dd1750dce61de19d2da58487c68ebfc
rvbrakel <rikvbrakel@hotmail.com>
2019-01-05 12:18:41 +0100

    Nu met commentaar

--- type.py
+++ type.py
@@ -13,24 +13,31 @@ def find_n(s,ch):
     return [i for i, ltr in enumerate(s) if ltr == ch]
 
 def find_type(homogeneous, path):
-    pathstring = str(path)
+    pathstring = str(path) #path to the equation
     newline = ""
+
+    #The RegEx to find all homogeneous parts of the equation.
     hom = re.compile("((?:-|\+|)(?:|\d|\d\d|\d\d\d).s\(n-(?:\d|\d\d)\)|\S\((?:\d|\d\d|\d\d\d).(?:\d|\d\d|\d\d\d)\).s\(n-(?:\d|\d\d)\))")
 
     print("path = " + str(path))
     print("pathstring = " + str(pathstring))
+
+    #Reads the whole file and sets line 3 (the equation) as variable "line"
     with open(str(pathstring), "r") as f:
         for i in range(3):
             line = f.readline()
         print("line = " + line)
 
+    #Splits the first string on the = symbol, only the s(n)..= part remains
     eq = line.split("=", 1)[0]
     num = eq.split(")",1)[1]
 
+    #This if checks if there is a number in front of the = symbol. If so it highers/lowers all n-x parts with that amount
     if num != " ":
         newline = line
         num = int(num)
         totalcount = line.count("(n-")
+        #If it is a negative number, it starts in the back of the string with highering the numbers
         if num < 0:
             loopcount = 1
             while loopcount < totalcount:
@@ -43,6 +50,7 @@ def find_type(homogeneous, path):
                 #print(newstring)
                 newline = newline.replace(oldstring, newstring)
                 loopcount = loopcount+1
+        #Else if it is a positive number it starts lowering all n-x's starting in the front
         else:
             loopcount = 1
             while loopcount < totalcount:
@@ -62,25 +70,36 @@ def find_type(homogeneous, path):
     else:
         print("Not needed to higher/lower the n- values")
 
+    #If the values didnt need highering/lowering it sets newline to just line
     if newline == "":
         newline = line
 
-    newline = newline.replace(" ", "")
-    homogeneous = re.findall(hom, newline.strip())
+    newline = newline.replace(" ", "") #Remove spaces in new line
+    homogeneous = re.findall(hom, newline.strip())#Finds all the homogeneous parts using the RegEx at the top of the function
     print("homogeneous parts are : " + str(homogeneous))
 
-    nonhomogeneous_string = newline.replace("s(n)","")
-    fn_part_sn_string = re.findall(("\d\^n|\d\d\^n|\d\d\d\^n"),nonhomogeneous_string)
-    fn_part_sn_string = ''.join(fn_part_sn_string).replace('^n','')
+    nonhomogeneous_string = newline.replace("s(n)","") #Created the nonhomstring variable with s(n) removed from the string newline
+    fn_part_sn_string = re.findall(("\d\^n|\d\d\^n|\d\d\d\^n"),nonhomogeneous_string) #Finds the sn part in the nonhom string
+    fn_part_sn_string = ''.join(fn_part_sn_string).replace('^n','') #changes the variable to a string instead of a list
 
+    #If the nonhom string didnt have a part like this, it sets it to -1
     if not fn_part_sn_string:
         fn_part_sn_string = -1
     print("fn_part_sn_string = " + str(fn_part_sn_string))
 
+    theorem_boolean = True #The boolean which checks if theorem 6 is applicable
+
+    #This if statement checks if theorem 6 is applicable on the equation.
+    if "^(n-" in nonhomogeneous_string:
+        theorem_boolean = False
+    print(theorem_boolean)
+
+    #This loop removes the nonhomogenous string from the whole equation, leaving just the homogeneous part
     for strings in homogeneous:
         nonhomogeneous_string = nonhomogeneous_string.replace(strings,"").replace(',',"").replace("=","").strip()
 
     print("nonhomogeneous_string = " + nonhomogeneous_string)
+    #This variable pastes the nonhom string after the hom string, correctly ordering the equation
     homogeneous_string = newline.replace(nonhomogeneous_string,"").replace(',',"").strip()
     print("homogeneous_string = " + homogeneous_string)
 
@@ -92,11 +111,13 @@ def find_type(homogeneous, path):
     s_pos = []
     count = 0
 
+    #Finds all positions of the "n" characters, subtracts 2 from that position to find al "s" positions
     for j in index:
         s_pos.append(count)
         s_pos[count] = j - 2
         count = count+1
 
+    #This loops checks every second postition before every "n" character
     for k in s_pos:
         if newline[k] == "s" and homogeneous is not False:
             homogeneous = True
